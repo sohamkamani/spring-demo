170b10500398a09498556c4d3bf249b7bd681729
sohamkamani <sohamkamani@gmail.com>
2022-07-22 16:14:10 +0530

    removed unused code

--- src/main/java/com/sohamkamani/demo/DemoApplication.java
+++ src/main/java/com/sohamkamani/demo/DemoApplication.java
@@ -1,16 +1,8 @@
 package com.sohamkamani.demo;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,53 +34,4 @@ public class DemoApplication {
 		return "Echo: " + echoText;
 	}
 
-	// We can add a variable to the path by wrapping its name in curly braces
-	@GetMapping("/echo/{text}")
-	// The PathVariable annotation assigns the text form the actual request to the `echoText`
-	// argument
-	public String echoPath(@PathVariable(name = "text") String echoText) {
-		return "Echo in path: " + echoText;
-	}
-
-	@GetMapping("/large-cappuccino")
-	public Coffee largeCappuccino() {
-		Coffee largeCappuccino = new Coffee();
-		largeCappuccino.setMilk(true);
-		largeCappuccino.setSize("L");
-		return largeCappuccino;
-	}
-
-	// We can use @PostMapping to denote that this handled a POST request
-	// to the /coffee endpoint
-	@PostMapping("/coffee")
-	// We need to specify the @RequestBody annotation along with the `Coffee`
-	// class as the argument. The decoded JSON body is then stored in the `coffee` argument
-	// variable
-	public String createCoffee(@RequestBody Coffee coffee) {
-		return "Created coffee: " + coffee.toString();
-	}
-
-	@GetMapping("/echo/headers")
-	public String echoHeaders(@RequestHeader Map<String, String> headers) {
-		StringBuilder sb = new StringBuilder("Headers: \n");
-		for (Entry<String, String> header : headers.entrySet()) {
-			sb.append(header.getKey());
-			sb.append(":");
-			sb.append(header.getValue());
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
-
-	@GetMapping("/custom-header")
-	public String setCustomHeader(HttpServletResponse response) {
-		response.setHeader("X-Custom-Header", "Some-Custom-Value");
-		return "ok";
-	}
-
-	@GetMapping("/sample-error")
-	public String sampleError(HttpServletResponse response) {
-		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		return "error";
-	}
 }
